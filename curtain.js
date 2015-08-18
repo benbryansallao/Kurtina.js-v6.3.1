e984182e367ed4a1ac5e1a891f3144c98ef649ac
Ben Bryan Sallao <benbryansallao@users.noreply.github.com>
2015-08-18 18:11:07 +0800

    Update curtain.js

--- curtain.js
+++ curtain.js
@@ -1,158 +1,153 @@
 /** 
  *  Curtain.js v1.0.0 
- *  Curtain style Overlay transition 
+ *  Curtain-Style-Overlay-Transition 
  *
  *  Author: Ben Bryan Sallao 
  *  Copyright 2015 
  **/
 
-$(document).ready(function () { 
+$('document').ready(function () { /* DOM Ready */
 
-var $CurtainReanimation = $('button'); /* Change the button to another variable name = http://www.benbryansallao.com */
+var $TriggerAnimation = $('h2'),
+	CurtainTimerDefault = 100,
+	$request_Reanimation = $('button');
 
-var CurtainTimerDefault = 100; /* Curtain animation default timer and or time when to start the animation. At 100 or 10% of 1 second. */
-
- function CurtainPlayAnimation() {    /* Function where the curtain animation Start */
- setTimeout(CurtainPlayAnimation, 100);
- CurtainTimerDefault ++;  /* The time default was adding its value every milli-seconds */
+var $LoopedCurtainlayers$Ready = function() {
+		  playAnimationCurtainLayer2() || playAnimationCurtainLayer3() || playAnimationCurtainLayer4() || playAnimationCurtainLayer5()
+		  || playAnimationCurtainLayer6() || playAnimationCurtainLayer7() || playAnimationCurtainLayer8() || playAnimationCurtainLayer9() ||
+		  playAnimationCurtainLayer10(); };
+	
+$init$CurtainReady = (function() { /* On page open (index.html) this function will be called automatically. */
+    Outloading();
+	_CurtainreAnimation();
+	});
  
- if(CurtainTimerDefault>100) {  
-  setTimeout(PlayCurtainLayer1, 100); /* set timeout to use on every frame of animation was 100 milli-seconds */
-  CurtainTimerDefault = 1000;  /* The time duration of the whole animation was 1000 or 1 second */
-  Play2();
-  Play3();
-  Play4();
-  Play5();
-  Play6();
-  Play7();
-  Play8();
-  Play9();
-  Play10();
- };
+ function CurtainPlayAnimation() {
+	 setTimeout(CurtainPlayAnimation, 100);
+	 CurtainTimerDefault ++;
+ 
+ if(CurtainTimerDefault>100) {
+	  setTimeout(PlayCurtainLayer1, 100);
+	  $LoopedCurtainlayers$Ready();
+	  CurtainTimerDefault = 1000;
+	 };
  };
 
-
-$('h2').click(function() { 
- CurtainPlayAnimation();
+$TriggerAnimation.click(function() {
+	 Inloading(), CurtainPlayAnimation();
  });
  
- init:CurtainReady = (function() { /* Looping all functions to be ready on page(index.html) open */
-	_CurtainreAnimation();
-	_sixPacksMenu();
-});
-
-
 _sixPacksMenu = (function () {
 		$(".six-packs")
 	   .fadeIn({ "opacity" : "1" });
 });
 
-_CurtainreAnimation = (function() {
-  $("h2, .overlay-container, .curtain-layer-one, .curtain-layer-two, .curtain-layer-three, .curtain-layer-four, .curtain-layer-five, .curtain-layer-six, .curtain-layer-seven, .curtain-layer-eight, .curtain-layer-nine, .curtain-layer-ten").fadeIn({ "opacity" : "1" }, 400);
-	   });
+	_CurtainreAnimation = (function() {
+		  $("h2 && .overlay-container && .curtain-layer-one && .curtain-layer-two && .curtain-layer-three && .curtain-layer-four && .curtain-layer-five && .curtain-layer-six && .curtain-layer-seven && .curtain-layer-eight && .curtain-layer-nine && .curtain-layer-ten").fadeIn();
+	});
+	PlayCurtainLayer1 = (function () { 
+		   Outloading();
+		  $(".curtain-layer-one, h2, .six-packs").fadeOut();
+		  });	  
+	PlayCurtainLayer2 = (function () { 
+		   $(".curtain-layer-two").fadeOut();
+		   });
+	PlayCurtainLayer3 = (function () { 
+		   $(".curtain-layer-three").fadeOut();
+		   });
+	PlayCurtainLayer4 = (function () { 
+		   $(".curtain-layer-four").fadeOut();
+		   });
+	PlayCurtainLayer5 = (function () { 
+		   $(".curtain-layer-five").fadeOut();
+		   });
+	PlayCurtainLayer6 = (function () { 
+		   $(".curtain-layer-six").fadeOut();
+		   });
+	PlayCurtainLayer7 = (function () { 
+		   $(".curtain-layer-seven").fadeOut();
+		   });
+	PlayCurtainLayer8 = (function () { 
+		   $(".curtain-layer-eight").fadeOut(); 
+		   });
+	PlayCurtainLayer9 = (function () { 
+		   $(".curtain-layer-nine").fadeOut();
+		   });
+	PlayCurtainLayer10 = (function () { 
+		   $(".curtain-layer-ten, .overlay-container").fadeOut();
+		   });
+ /* 
+  * In using of an event handler to animate this Curtain.js layers, it is advisable to use "fadeIn/Out" and not hide/show or remove/addClass, 
+  * because of the easing effects and transition results' discrepancies.
+  * However, on the version2 the function and format could be more intelligent.
+  */
+ 
+	function playAnimationCurtainLayer2() {
+		 if(CurtainTimerDefault>200) {
+		  setTimeout(PlayCurtainLayer2, 200);
+		  
+		 };
+	};
+	function playAnimationCurtainLayer3() {
+		 if(CurtainTimerDefault>300) {
+		  setTimeout(PlayCurtainLayer3, 300);
 
+		 }; 
+	};
+	function playAnimationCurtainLayer4() {
+		 if(CurtainTimerDefault>400) {
+		  setTimeout(PlayCurtainLayer4, 400);
 
-PlayCurtainLayer1 = (function () { 
-	   Outloading();
-	  $(".curtain-layer-one, h2, .six-packs").fadeOut();
-	  });
-PlayCurtainLayer2 = (function () { 
-	   $(".curtain-layer-two").fadeOut();
-	   });
-PlayCurtainLayer3 = (function () { 
-	   $(".curtain-layer-three").fadeOut();
-	   });
-PlayCurtainLayer4 = (function () { 
-	   $(".curtain-layer-four").fadeOut();
-	   });
-PlayCurtainLayer5 = (function () { 
-	   $(".curtain-layer-five").fadeOut();
-	   });
-PlayCurtainLayer6 = (function () { 
-	   $(".curtain-layer-six").fadeOut();
-	   });
-PlayCurtainLayer7 = (function () { 
-	   $(".curtain-layer-seven").fadeOut();
-	   });
-PlayCurtainLayer8 = (function () { 
-	   $(".curtain-layer-eight").fadeOut(); 
-	   });
-PlayCurtainLayer9 = (function () { 
-	   $(".curtain-layer-nine").fadeOut();
-	   });
-PlayCurtainLayer10 = (function () { 
-	   $(".curtain-layer-ten, .overlay-container").fadeOut();
-	   });
-	   
-	   
+		 };
+	};
+	function playAnimationCurtainLayer5() {
+		 if(CurtainTimerDefault>500) {
+		  setTimeout(PlayCurtainLayer5, 500);
+
+		 }; 
+	};
+	function playAnimationCurtainLayer6() {
+		 if(CurtainTimerDefault>600) {
+		  setTimeout(PlayCurtainLayer6, 600);
+
+		 }; 
+	};
+	function playAnimationCurtainLayer7() {
+		 if(CurtainTimerDefault>700) {
+		  setTimeout(PlayCurtainLayer7, 700);
+
+		 }; 
+	};
+	function playAnimationCurtainLayer8() {
+		 if(CurtainTimerDefault>800) {
+		  setTimeout(PlayCurtainLayer8, 800);
+
+		 }; 
+	};
+	function playAnimationCurtainLayer9() {
+		 if(CurtainTimerDefault>900) {
+		  setTimeout(PlayCurtainLayer9, 900);
+
+		 }; 
+	};
+	function playAnimationCurtainLayer10() {
+		 if(CurtainTimerDefault>1000) {
+		  setTimeout(PlayCurtainLayer10, 1000);
+
+		 }; 
+	};
 	
-
- 
- function Play2() {
- if(CurtainTimerDefault>200) {
-  setTimeout(PlayCurtainLayer2, 200);
-  
-  
- }; };
- 
- function Play3() {
- if(CurtainTimerDefault>300) {
-  setTimeout(PlayCurtainLayer3, 300);
-
- }; };
- 
- function Play4() {
- if(CurtainTimerDefault>400) {
-  setTimeout(PlayCurtainLayer4, 400);
-
- }; };
- 
- function Play5() {
- if(CurtainTimerDefault>500) {
-  setTimeout(PlayCurtainLayer5, 500);
-
- }; };
- 
- function Play6() {
- if(CurtainTimerDefault>600) {
-  setTimeout(PlayCurtainLayer6, 600);
-
- }; };
- 
- function Play7() {
- if(CurtainTimerDefault>700) {
-  setTimeout(PlayCurtainLayer7, 700);
-
- }; };
- 
- function Play8() {
- if(CurtainTimerDefault>800) {
-  setTimeout(PlayCurtainLayer8, 800);
-
- }; };
- 
- function Play9() {
- if(CurtainTimerDefault>900) {
-  setTimeout(PlayCurtainLayer9, 900);
-
- }; };
- 
- function Play10() {
- if(CurtainTimerDefault>1000) {
-  setTimeout(PlayCurtainLayer10, 1000);
-
- }; };
-
 function Inloading() {
-$(".loading").fadeIn({ "oppacity" : "1" });
+$(".loading").fadeIn();
 };
 function Outloading() {
-$(".loading").fadeOut({});
+$(".loading").fadeOut();
 };
-	
- $CurtainReanimation.click(function Reanimate() { /* When request reanimation, this function will be called the CurtainReady like on window load */
-   init:CurtainReady();	
-   Reanimate();
+
+ $request_Reanimation.click(function Reanimate () {
+ var $url = function() { window.location.href='index.html'}; /* Request curtain re-animation (function to reload the page)*/
+    $url() && Outloading();
+	die();
 });
 
 }) ();

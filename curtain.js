3a9d4f0c8a7fafae40672a0eeab64ed12efdd223
FISH or FIST? <benbryansallao@users.noreply.github.com>
2016-03-29 20:13:19 +0800

    Update curtain.js

--- curtain.js
+++ curtain.js
@@ -1,5 +1,5 @@
 /** 
- *  Curtain.js v2.1.0 
+ *  Curtain.js v2.2.1 
  *  Curtain style Overlay transition 
  *
  *  Author: Ben Bryan Sallao 
@@ -9,16 +9,17 @@
 /* About Curtain.js the exact curtain/blinds simulation from the house of Tony Stark in the Iron Man1 Movie */
 
 $(document).ready(function () { 
-var tr=document.getElementById('reanimate'),
-tf=document.getElementById('animation'),
+var r=document.getElementById('reanimate'),
+f=document.getElementById('animation'),
 button=$('button'),
 to=$('h2'),
 n=["100"],
 cm=n,
-el=function(){ee||dd||ff},
+i=1,
+el=function(){e||d||f},
 o=function()
 {    
- on(o,100);cm++;if(cm>=100){on(pa,100),cm=1000,ab();};
+ on(o,cm),cm++;if(cm>=cm&&cm===101){on(pa,cm),cm=1000,ab();};
  };
 var es=to;es.click(function(){o();});x=(function(){cr();});cr=(function(){xd;});
 var istudyononatoReady=x;ab=function(){if(cm>=200){on(pb,200)}if(cm>300){on(pc,300)}if(cm>=400){on(pd,400)}if(cm>500){on(pe,500);}if(cm>600){on(pf,600);}if(cm>700){on(pg,700);}if(cm>800){on(ph,800);}if(cm>900){on(pi,900);}if(cm>=1000){on(pj,1000);}};var xd = $("h2, .overlay-container, .curtain-layer-one, .curtain-layer-two, .curtain-layer-three, .curtain-layer-four, .curtain-layer-five, .curtain-layer-six, .curtain-layer-seven, .curtain-layer-eight, .curtain-layer-nine, .curtain-layer-ten").fadeIn();var pja=$(".curtain-layer-ten, .overlay-container"),pia=$(".curtain-layer-nine"),pha=$(".curtain-layer-eight"),pga=$(".curtain-layer-seven"),pfa=$(".curtain-layer-six"),pea=$(".curtain-layer-five"),pda=$(".curtain-layer-four"),pca=$(".curtain-layer-three"),pba=$(".curtain-layer-two"),paa=$(".curtain-layer-one, h2"),on=setTimeout; pa=(function(){paa.fadeOut();});pb=(function(){pba.fadeOut();});pc=(function(){pca.fadeOut();});pd=(function(){pda.fadeOut();});pe=(function(){pea.fadeOut();});pf=(function (){pfa.fadeOut();});pg=(function (){pga.fadeOut();});ph=(function(){pha.fadeOut()});pi=(function(){pia.fadeOut()});pj=function(){pja.fadeOut();};var wh=button;wh.click(function(){window.location.href='index.html';});

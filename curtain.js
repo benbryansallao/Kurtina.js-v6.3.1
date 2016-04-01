5afc6b473a7639dd14deed9b82030ef18563c747
FISH or FIST? <benbryansallao@users.noreply.github.com>
2016-04-01 14:01:34 +0800

    Update curtain.js

--- curtain.js
+++ curtain.js
@@ -1,29 +1,31 @@
 /** 
- *  Curtain.js v3.2.0 
+ *  Curtain.js v4.2.1 
  *  Curtain-Style-Overlay-Transition 
  *
  *  Author: Ben Bryan Sallao 
  *  Copyright 2015-2016
  *  Istudyo Nonato
+ *
+ *  About Curtain.js the exact curtain/blinds simulation from the house of Tony Stark in the Iron Man1 Movie
  **/
-/* About Curtain.js the exact curtain/blinds simulation from the house of Tony Stark in the Iron Man1 Movie */
 
 $(document).ready(function () { 
 var r=document.getElementById('reanimate'),
-f=document.getElementById('animation'),
 button=$('button'),
 to=$('h2'),
 n=["100"],
 cm=n,
-g=Math.round(cm*cm)/10,
-h=Math.ceil(50.1),
-ab=function(){p1 || p2 || p3 || p4 || p5 || p6 || p7 || p8|| p9;},
+/* Mathematical computation on Curtain/blinds animation */
+y= Math.sqrt(cm), 
+g= Math.round(cm*cm)/10,
+h= Math.ceil(49.13), 
+kp= Math.pow(h,2)-g-g+l,l=Math.round(n,2)*2,xk=Math.round(h*2)+l,km=Math.round(h*2)+xk,kn=Math.round(n,2)+l+xk-n,ko=Math.pow(cm,2)/10-km,kq=Math.round(km*2),kr=Math.round(l*2)+kn,
+/* Mathematical computation on Curtain/blinds animation */
 o=function()
 {    
- on(o,cm),cm++;if(cm>=cm){on(pa,cm),cm=g,ab;};
- 
+ on(o,cm),cm++;if(cm>=cm){on(pa,cm),cm=g,ab();};
  };
 var es=to;es.click(function(){o();});x=(function(){cr();});cr=(function(){xd;});
 var istudyononatoReady=x;
-var p1=function() {if(cm>kk){on(pb,kk);var kk=Math.pow(h,2)-g-g-400.10;}};var p2=function() {if(cm>kl){on(pc,kl);var kl=Math.pow(cm,2)/cm-700;}};var p3=function() {if(cm>km){on(pd,km);var km=Math.pow(cm,2)/cm-600;}};var p4=function() {if(cm>kn){on(pe,kn);var kn=Math.pow(cm,2)/cm-500;}alert(kn);};var p5=function() {if(cm=>ko){on(pf,ko);var ko=Math.pow(cm,2)/cm-400;}};var p6=function() {if(cm>kp){on(pg,kp);var kp=Math.pow(cm,2)/cm-300;}};var p7=function() {if(cm>kq){on(ph,kq);}var kq=Math.pow(cm,2)/cm-200;};var p8=function() {if(cm>kr){on(pi,kr);}var kr=Math.pow(cm,2)/cm-cm;};var p9=function() {if(cm=>g){on(pj,g);}};var xd = $("h2, .overlay-container, .curtain-layer-one, .curtain-layer-two, .curtain-layer-three, .curtain-layer-four, .curtain-layer-five, .curtain-layer-six, .curtain-layer-seven, .curtain-layer-eight, .curtain-layer-nine, .curtain-layer-ten").fadeIn();var pja=$(".curtain-layer-ten, .overlay-container"),pia=$(".curtain-layer-nine"),pha=$(".curtain-layer-eight"),pga=$(".curtain-layer-seven"),pfa=$(".curtain-layer-six"),pea=$(".curtain-layer-five"),pda=$(".curtain-layer-four"),pca=$(".curtain-layer-three"),pba=$(".curtain-layer-two"),paa=$(".curtain-layer-one, h2"),on=setTimeout; pa=(function(){paa.fadeOut();});pb=(function(){pba.fadeOut();});pc=(function(){pca.fadeOut();});pd=(function(){pda.fadeOut();});pe=(function(){pea.fadeOut();});pf=(function (){pfa.fadeOut();});pg=(function (){pga.fadeOut();});ph=(function(){pha.fadeOut()});pi=(function(){pia.fadeOut()});pj=function(){pja.fadeOut();};var wh=button;wh.click(function(){window.location.href='index.html';});
+ab=function() {if(cm>l){on(pb,l);}if(cm>xk){on(pc,xk);};if(cm>km){on(pd,km);};if(cm>kn){on(pe,kn);};if(cm=>ko){on(pf,ko);};if(cm>kp){on(pg,kp);};if(cm>kq){on(ph,kq);};if(cm>kr){on(pi,kr);};if(cm=>g){on(pj,g);};};var xd = $("h2, .overlay-container, .curtain-layer-one, .curtain-layer-two, .curtain-layer-three, .curtain-layer-four, .curtain-layer-five, .curtain-layer-six, .curtain-layer-seven, .curtain-layer-eight, .curtain-layer-nine, .curtain-layer-ten").fadeIn();var pja=$(".curtain-layer-ten, .overlay-container"),pia=$(".curtain-layer-nine"),pha=$(".curtain-layer-eight"),pga=$(".curtain-layer-seven"),pfa=$(".curtain-layer-six"),pea=$(".curtain-layer-five"),pda=$(".curtain-layer-four"),pca=$(".curtain-layer-three"),pba=$(".curtain-layer-two"),paa=$(".curtain-layer-one, h2"),on=setTimeout; pa=(function(){paa.fadeOut();});pb=(function(){pba.fadeOut();});pc=(function(){pca.fadeOut();});pd=(function(){pda.fadeOut();});pe=(function(){pea.fadeOut();});pf=(function (){pfa.fadeOut();});pg=(function (){pga.fadeOut();});ph=(function(){pha.fadeOut()});pi=(function(){pia.fadeOut()});pj=function(){pja.fadeOut();};var wh=button;wh.click(function(){window.location.href='index.html';});
 })();

8446896258b42106ac0291641a262185ce6dd29b
FISH or FIST? <benbryansallao@users.noreply.github.com>
2016-04-03 17:49:51 +0800

    Update curtain.js

--- curtain.js
+++ curtain.js
@@ -8,7 +8,6 @@
  *
  *  About Curtain.js the exact curtain/blinds simulation from the house of Tony Stark in the Iron Man1 Movie
  **/
-
 $(document).ready(function () {  /* DOM Ready */
 var r=document.getElementById('reanimate'),
 button=$('button'),
@@ -30,5 +29,7 @@ o=function()
 
 var es=to;es.click(function(){o();});x=(function(){cr();});cr=(function(){xd;});
 var istudyononatoReady=x;
+
+/* Discrete functions to constitute simulation with its mathematical computation equivalent */
 ab=function() {if(cm>l){on(pb,l);}if(cm>xk){on(pc,xk);};if(cm>km){on(pd,km);};if(cm>kn){on(pe,kn);};if(cm=>ko){on(pf,ko);};if(cm>kp){on(pg,kp);};if(cm>kq){on(ph,kq);};if(cm>kr){on(pi,kr);};if(cm=>g){on(pj,g);};};var xd = $("h2, .overlay-container, .curtain-layer-one, .curtain-layer-two, .curtain-layer-three, .curtain-layer-four, .curtain-layer-five, .curtain-layer-six, .curtain-layer-seven, .curtain-layer-eight, .curtain-layer-nine, .curtain-layer-ten").fadeIn();var pja=$(".curtain-layer-ten, .overlay-container"),pia=$(".curtain-layer-nine"),pha=$(".curtain-layer-eight"),pga=$(".curtain-layer-seven"),pfa=$(".curtain-layer-six"),pea=$(".curtain-layer-five"),pda=$(".curtain-layer-four"),pca=$(".curtain-layer-three"),pba=$(".curtain-layer-two"),paa=$(".curtain-layer-one, h2"),on=setTimeout; pa=(function(){paa.fadeOut();});pb=(function(){pba.fadeOut();});pc=(function(){pca.fadeOut();});pd=(function(){pda.fadeOut();});pe=(function(){pea.fadeOut();});pf=(function (){pfa.fadeOut();});pg=(function (){pga.fadeOut();});ph=(function(){pha.fadeOut()});pi=(function(){pia.fadeOut()});pj=function(){pja.fadeOut();};var wh=button;wh.click(function(){window.location.href='index.html';});
 })();
